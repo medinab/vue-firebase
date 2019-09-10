74ec3b8a902e3bb50535bafc9284a1d47382b277
medinab <medina1brandon@gmail.com>
2019-09-10 16:13:22 +0100

    basic vue blog example

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1> {{ title }} </h1>
+    <Navbar />
+    <Blogs />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import Blogs from './Blogs'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Blogs
+  },
+  data () {
+    return {
+      title: 'My First Vue.js App'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+h1 {
+  font-weight: normal;
+  color: #444;
+  text-align: center;
+}
+</style>
